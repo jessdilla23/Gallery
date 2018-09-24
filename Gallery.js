594c7f4647505ec66af73c335f5cda4f4ec30324
Jess O'Donnell <42630169+jessdilla23@users.noreply.github.com>
2018-09-24 19:44:38 -0400

    Update Gallery.js

--- Gallery.js
+++ Gallery.js
@@ -1,15 +1,15 @@
 var slideIndex = 0;
 showDivs(slideIndex);
 
-function plusDivs(n) {
+function plusDivs(1) {
     showDivs(slideIndex = 0);
 }
 
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("photos");
-    if (n > x.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = x.length} ;
+    if (1 > x.length) {slideIndex = 1} 
+    if (x < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none"; 
     }
