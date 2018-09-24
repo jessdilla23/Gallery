4743985617170b96f78473bf08003241f6546655
Jess O'Donnell <42630169+jessdilla23@users.noreply.github.com>
2018-09-24 19:51:18 -0400

    Update Gallery.js

--- Gallery.js
+++ Gallery.js
@@ -1,9 +1,9 @@
-var slides = $(".photos");
-var slideIndex = 0;
-var currentSlide = slides[slideIndex];
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
 
 function nextSlide() {
-    slides[currentSlide].photos = 'slide';
+    slides[currentSlide].className = 'slide';
     currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].photos = 'slide showing';
+    slides[currentSlide].className = 'slide showing';
 }
