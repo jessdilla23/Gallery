66bd7a097dc81af89c082916de630cdc79fcc614
Jess O'Donnell <42630169+jessdilla23@users.noreply.github.com>
2018-09-24 21:55:15 -0400

    Update Gallery.js

--- Gallery.js
+++ Gallery.js
@@ -1,16 +1,3 @@
 var slides = $(".photos");
 var slideIndex = 0;
-var currentSlide = slideshow[photoindexIndex];
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("photos");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 5000);
-}
+var currentSlide = slides[slideIndex];
