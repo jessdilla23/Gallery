b59d503bd729c3ac60a1295dcf12a171c8f0bed0
Jess O'Donnell <42630169+jessdilla23@users.noreply.github.com>
2018-09-24 22:17:33 -0400

    Update Gallery.js

--- Gallery.js
+++ Gallery.js
@@ -1,3 +1,29 @@
+/*
+I tried to write many different types of JavaScript to complete the slideshow function.
+I've left part of my most recent attempt in comments (i've committed this project probably near 100 times and lost a lot of work)
+but the only JavaScript code that showed my photos 
+was this automatic slide show from W3 Schools.
+I have included it here to complete the project and made some edits
+*/
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("photos");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 5000); 
+    // This will change the image every 5 seconds
+}
+
+
+/*
 var slides = $(".slides");
 var slideIndex = 0;
 var currentSlide = slides[slideIndex];
@@ -9,3 +35,4 @@ $("#hide").click(function(){
 $("#show").click(function(){
     $("p").show();
 });
+*/
